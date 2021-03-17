e7c3804bbe2623760a9975678ec1493f9bf1fa5e
salkorad <salko.radovovic@gmail.com>
2021-03-17 03:26:34 +0100

    Boolean Values

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+function myTrueBoolean() {
+    return true;
+}
+console.log(myTrueBoolean());
+// Only change code below this line
+module.exports = myTrueBoolean;
