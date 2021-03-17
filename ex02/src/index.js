2d40131ba824270bb4be3b21924b3c34d82183ab
salkorad <salko.radovovic@gmail.com>
2021-03-17 03:27:33 +0100

    If statements

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function myFunction(myParameter) {
+    if(myParameter == true) {
+        return "The parameter is true!";
+    }  
+    return "The parameter is false!";  
+}
+console.log(myFunction(true));
+console.log(myFunction(false));
+console.log(myFunction(true));
+console.log(myFunction(false));
+// Only change code below this line
+module.exports = myFunction;
